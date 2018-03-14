f15460530739c0facf9985fa0517b26db4333ef9
Chelsea Zhu <qianwei5668@gmail.com>
2018-03-13 21:33:23 -0700

    not working

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import AddGameForm from './AddGameForm';
 import GameList from './GameList';
 import GameDetail from './GameDetail';
-import AddGameForm from './AddGameForm';
+
+//import CardGame from '../models/cardGame.js';
 
 var bodyParser = require('body-parser')
+var CardGame = require('../models/cardGame')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -52,8 +55,31 @@ class App extends Component {
     })
   }
   handleSubmit(e) {
-    // Add your code here to handle adding a new game to the database
+    console.log(this.state)
+    e.preventDefault();
+
+    var newGame = CardGame({
+      name: this.state.nameVal,
+      minPlayers: this.state.minVal,
+      maxPlayers: this.state.maxVal
+    });
+
+    // write to DB
+    newGame.save(function(err) {
+      if (err) return console.log(err);
+      console.log('newGame created!');
+    })
+
+    //set the front end to show updated list
+    this.setState({
+      games: [...games,{
+        nameVal: this.state.nameVal,
+        minVal: this.state.minVal,
+        maxVal: this.state.maxVal
+      }];
+    })
   }
+  
 
   // API call goes here so that data is available after component mounts
   componentDidMount() {
@@ -70,10 +96,10 @@ class App extends Component {
         <GameList onGameClick={this.handleGameClick} games={this.state.games} />
         <GameDetail game={this.state.current} />
         <AddGameForm
-          onNameChange={this.handleNameChange}
-          onMinChange={this.handleMinChange}
-          onMaxChange={this.handleMaxChange}
-          onSubmit={this.handleSubmit} />
+          onNameChange=this.handleNameChange
+          onMinChange=this.handleMinChange
+          onMaxChange=this.handleMaxChange
+          onSubmit=this.handleSubmit />
       </div>
     );
   }
